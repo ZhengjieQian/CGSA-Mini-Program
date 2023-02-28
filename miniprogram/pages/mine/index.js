6c4d9c0c8717e13dee2a77248f182f864f2617ff
RatsCommander <jasonjiachengsong@163.com>
2023-02-27 17:55:23 -0800

    Add resources page

--- miniprogram/pages/mine/index.js
+++ miniprogram/pages/mine/index.js
@@ -1,66 +1,24 @@
 // pages/mine/index.js
+// 用户个人信息页面
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    hasUserInfo: false,
+    promotionCardBackgroundImage: '../../images/mine/promotion_card_background.jpg',
+    avatarDefaultImage: '../../images/mine/avatar_default.png'
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  getUserProfile(e) {
+    wx.getUserProfile({
+      desc: '用于完善会员资料',
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        });
+      }
+    });
   }
-})
+});
--- miniprogram/pages/resources/index.js
+++ miniprogram/pages/resources/index.js
@@ -1,66 +1,33 @@
 // pages/resources/index.js
+// 资源页
 Page({
-
   /**
    * 页面的初始数据
+   * topImage 页面的头图
+   * resources JSON 数组，每个 JSON 对象都是一条资源
    */
   data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+    topImage: '../../images/resources/resources_top.jpg',
+    resources: [{
+      id: 0,
+      title: '# 【CGSA新媒体部 | Trojan宝典】LA出游篇 #',
+      image: '../../images/resources/resources1.jpg'
+    }, {
+      id: 1,
+      title: '# 【USC薅羊毛篇】是时候告诉你这些省钱秘籍啦 #',
+      image: '../../images/resources/resources2.jpg'
+    }, {
+      id: 2,
+      title: '# CGSA 干货 | 如何在夏天到来前练出八块腹肌 #',
+      image: '../../images/resources/resources3.jpg'
+    }, {
+      id: 3,
+      title: '# LA留守儿童宝典(1) #',
+      image: '../../images/resources/resources4.jpg'
+    }, {
+      id: 4,
+      title: '# 全美肺炎确诊破20万！宅家囤货宝典快收藏起来吧！',
+      image: '../../images/resources/resources5.jpg'
+    }]
   }
-})
+});

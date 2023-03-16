cd79f7a63d45268a983d706718f2fd77a3fc284f
RatsCommander <jasonjiachengsong@163.com>
2023-03-15 17:07:28 -0700

    Update get promo card page

--- miniprogram/pages/get-promo-card/get-promo-card.js
+++ miniprogram/pages/get-promo-card/get-promo-card.js
@@ -5,6 +5,20 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    genderPickerIndex: '',
+    genderPickerData: ['男', '女'],
+    birthday: '',
+    birthdayPickerStartDate: '1900-01-01',
+    birthdayPickerEndDate: '2023-03-15'
+  },
+  genderPickerChanged(e) {
+    this.setData({
+      genderPickerIndex: e.detail.value
+    });
+  },
+  birthdayPickerChanged(e) {
+    this.setData({
+      birthday: e.detail.value
+    });
   }
 });

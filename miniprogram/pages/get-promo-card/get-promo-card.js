c1b5b9c04a31a27553e9ca32e21def8ca3804a87
RatsCommander <jasonjiachengsong@163.com>
2023-03-16 15:35:40 -0700

    Add input handlers on get promo card page

--- miniprogram/pages/get-promo-card/get-promo-card.js
+++ miniprogram/pages/get-promo-card/get-promo-card.js
@@ -5,20 +5,49 @@ Page({
    * 页面的初始数据
    */
   data: {
+    name: '',
     genderPickerIndex: '',
-    genderPickerData: ['男', '女'],
+    gender: '',
+    genders: ['男', '女'],
+    email: '',
+    phone: '',
+    university: '',
     birthday: '',
     birthdayPickerStartDate: '1900-01-01',
     birthdayPickerEndDate: '2023-03-15'
   },
+  nameInput(e) {
+    this.setData({
+      name: e.detail.value
+    });
+  },
   genderPickerChanged(e) {
     this.setData({
-      genderPickerIndex: e.detail.value
+      genderPickerIndex: e.detail.value,
+      gender: this.data.genders[e.detail.value]
+    });
+  },
+  emailInput(e) {
+    this.setData({
+      email: e.detail.value
+    });
+  },
+  phoneInput(e) {
+    this.setData({
+      phone: e.detail.value
+    });
+  },
+  universityInput(e) {
+    this.setData({
+      university: e.detail.value
     });
   },
   birthdayPickerChanged(e) {
     this.setData({
       birthday: e.detail.value
     });
+  },
+  submitInfo() {
+    console.log(this.data);
   }
 });

0d9e36afcbf209d5ad1e212c07ea859eb75b029a
RatsCommander <jasonjiachengsong@163.com>
2023-03-27 15:35:05 -0700

    Update promotional card page

--- miniprogram/pages/promo-card/promo-card.js
+++ miniprogram/pages/promo-card/promo-card.js
@@ -5,6 +5,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    showIndicatorDots: true,
+    indicatorColor: '#fff6eb',
+    indicatorActiveColor: '#ffffff',
+    autoPlay: true,
+    interval: 5000,
+    duration: 1300,
+    images: [
+      '../../images/index-swiper/index-swiper1.jpg',
+      '../../images/index-swiper/index-swiper2.jpg',
+      '../../images/index-swiper/index-swiper3.jpg',
+      '../../images/index-swiper/index-swiper4.jpg'
+    ],
   },
 });

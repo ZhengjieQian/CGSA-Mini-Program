882d2f983091c3a4649038537ee27af29f229bc8
RatsCommander <jasonjiachengsong@163.com>
2023-04-04 14:17:09 -0700

    Added no post component on user post page

--- miniprogram/pages/my-posts/my-posts.js
+++ miniprogram/pages/my-posts/my-posts.js
@@ -6,6 +6,10 @@ Page({
    */
   data: {
     topTabPublishedIcon: '../../images/my-post/top_tab_published_active.jpg',
-    topTabRemovedIcon: '../../images/my-post/top_tab_removed_inactive.jpg'
+    topTabRemovedIcon: '../../images/my-post/top_tab_removed_inactive.jpg',
+    noPostImage: '../../images/my-post/no_post.jpg'
   },
+  postBtnTapped() {
+    console.log('post');
+  }
 });

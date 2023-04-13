a493e4b7b4653f62598733685ab2f064faf6f26d
RatsCommander <jasonjiachengsong@163.com>
2023-04-13 14:25:48 -0700

    Update topic part on publish alumni posts page

--- miniprogram/pages/publish-alumni-posts/publish-alumni-posts.js
+++ miniprogram/pages/publish-alumni-posts/publish-alumni-posts.js
@@ -5,7 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    forwardArrow: '../../images/publish-alumni-posts/forward_arrow.jpg'
+  },
+  topicTapped() {
+    console.log('选择话题');
   },
   postBtnTapped() {
     console.log('发布');

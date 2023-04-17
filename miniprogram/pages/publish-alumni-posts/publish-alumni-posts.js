cd7e54c3723f9965dfa8bbf83b2aada89be022a3
RatsCommander <jasonjiachengsong@163.com>
2023-04-17 13:52:09 -0700

    Update textarea on publish alumni posts page

--- miniprogram/pages/publish-alumni-posts/publish-alumni-posts.js
+++ miniprogram/pages/publish-alumni-posts/publish-alumni-posts.js
@@ -5,7 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    forwardArrow: '../../images/publish-alumni-posts/forward_arrow.jpg'
+    forwardArrow: '../../images/publish-alumni-posts/forward_arrow.jpg',
+    upArrow: '../../images/publish-alumni-posts/up_arrow.jpg',
+    downArrow: '../../images/publish-alumni-posts/down_arrow.jpg',
+    crossArrow: '../../images/publish-alumni-posts/cross_arrow.jpg'
   },
   topicTapped() {
     console.log('选择话题');

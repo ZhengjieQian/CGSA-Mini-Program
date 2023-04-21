b2fae7f6f216122d08fe2f292bbf8d117677a02b
RatsCommander <jasonjiachengsong@163.com>
2023-04-21 14:03:52 -0700

    Add hot topic list on topic selection page

--- miniprogram/pages/topic-selection/topic-selection.js
+++ miniprogram/pages/topic-selection/topic-selection.js
@@ -3,9 +3,13 @@
 Page({
   data: {
     searchIcon: '../../images/topic-selection/search_icon.jpg',
-    cancelIcon: '../../images/topic-selection/cancel_icon.jpg'
+    cancelIcon: '../../images/topic-selection/cancel_icon.jpg',
+    topics: ['表白墙', '吐槽', '曝光', '学术交流']
   },
   cancelSearch() {
     console.log('取消')
+  },
+  topicSelected(e) {
+    console.log(this.data.topics[e.currentTarget.dataset.topicIndex]);
   }
 });

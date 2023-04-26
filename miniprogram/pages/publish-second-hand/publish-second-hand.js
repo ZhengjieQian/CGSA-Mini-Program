2b0b1de4f1c6d68957e9719d2ddbd472e837ec63
RatsCommander <jasonjiachengsong@163.com>
2023-04-26 13:39:06 -0700

    Add detail section on publish second hand page

--- miniprogram/pages/publish-second-hand/publish-second-hand.js
+++ miniprogram/pages/publish-second-hand/publish-second-hand.js
@@ -5,9 +5,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-    addImage: '../../images/publish-second-hand/add_image.jpg'
+    addImage: '../../images/publish-second-hand/add_image.jpg',
+    forwardArrow: '../../images/publish-second-hand/forward_arrow.jpg'
   },
   addImage() {
     console.log('添加图片');
+  },
+  selectCategory() {
+    console.log('选择类别');
+  },
+  inputPrice() {
+    console.log('输入价格');
+  },
+  inputContactInfo() {
+    console.log('输入联系方式');
   }
 })

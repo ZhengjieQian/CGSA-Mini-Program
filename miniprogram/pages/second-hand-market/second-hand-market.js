aa61f8aed86684db898b16947ae96a7ecf908693
RatsCommander <jasonjiachengsong@163.com>
2023-05-04 21:07:32 -0700

    Update selecting filter second hand market page

--- miniprogram/pages/second-hand-market/second-hand-market.js
+++ miniprogram/pages/second-hand-market/second-hand-market.js
@@ -2,6 +2,7 @@
 // 二手市场
 Page({
   data: {
-    searchIcon: '../../images/second-hand-market/search_icon.jpg'
+    searchIcon: '../../images/second-hand-market/search_icon.jpg',
+    filterItemIcon: '../../images/second-hand-market/filter_item_icon.jpg'
   },
 });

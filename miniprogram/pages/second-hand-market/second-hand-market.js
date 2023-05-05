c8d2c8a78f98c4b3aa87758f29438da24c9cb01f
RatsCommander <jasonjiachengsong@163.com>
2023-05-05 13:47:13 -0700

    Update second hand market page

--- miniprogram/pages/second-hand-market/second-hand-market.js
+++ miniprogram/pages/second-hand-market/second-hand-market.js
@@ -3,6 +3,13 @@
 Page({
   data: {
     searchIcon: '../../images/second-hand-market/search_icon.jpg',
-    filterItemIcon: '../../images/second-hand-market/filter_item_icon.jpg'
+    filterItemIcon: '../../images/second-hand-market/filter_item_icon.jpg',
+    tags: ['全部', '电子产品', '交通工具', '家具家电', '厨房用品', '学习资料'],
+    activeTagIndex: 0
   },
+  changeTag(e) {
+    this.setData({
+      activeTagIndex: e.currentTarget.dataset.currentIndex
+    });
+  }
 });

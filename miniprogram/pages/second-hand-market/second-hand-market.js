18b27c5bb94858dea75959fc1d7d927edf3956a2
shulalazzz <zj2060203064@gmail.com>
2023-06-09 13:34:59 -0700

    added customize price range in second hand market

--- miniprogram/pages/second-hand-market/second-hand-market.js
+++ miniprogram/pages/second-hand-market/second-hand-market.js
@@ -2,6 +2,8 @@
 // 二手市场
 Page({
     data: {
+        minPrice: "",
+        maxPrice: "",
         comprehensiveSortValue: 0,
         comprehensiveSortOptions: [
             { text: "综合排序", value: 0, icon: "" },
@@ -9,6 +11,7 @@ Page({
             { text: "价格降序", value: 2, icon: "" },
             { text: "价格升序", value: 3, icon: "" },
         ],
+        priceTitle: "",
         priceSortValue: 0,
         priceSortOptions: [
             { text: "价格不限", value: 0, icon: "" },
@@ -97,6 +100,60 @@ Page({
                 date: "04-26",
             },
         ],
+    },
+    onReady() {
+        this.setData({
+            priceTitle: this.data.priceSortOptions[0].text,
+        });
+    },
+    // minPriceInput(e) {
+    //     this.setData({
+    //         minPrice: e.detail.value,
+    //     });
+    // },
+    // maxPriceInput(e) {
+    //     this.setData({
+    //         maxPrice: e.detail.value,
+    //     });
+    // },
+    getPriceRange() {
+        if (!this.data.minPrice || !this.data.maxPrice) {
+            this.selectComponent("#price-range").toggle();
+            return;
+        }
+        if (parseInt(this.data.minPrice) > parseInt(this.data.maxPrice)) {
+            let min = this.data.maxPrice;
+            let max = this.data.minPrice;
+            this.setData({
+                minPrice: min,
+                maxPrice: max,
+            });
+        }
+        console.log("价格范围" + this.data.minPrice + "-" + this.data.maxPrice);
+        this.setData({
+            priceTitle: "$" + this.data.minPrice + "-$" + this.data.maxPrice,
+        });
+        this.selectComponent("#price-range").toggle();
+    },
+    clearPrice() {
+        this.setData({
+            minPrice: "",
+            maxPrice: "",
+        });
+    },
+    updateTitle(e) {
+        // console.log(e);
+        if (!typeof e.detail === "number") {
+            return;
+        }
+        this.setData({
+            priceSortValue: e.detail,
+            priceTitle: this.data.priceSortOptions[e.detail].text,
+        });
+        // console.log(
+        //     "价格排序" +
+        //         this.data.priceSortOptions[this.data.priceSortValue].text
+        // );
     },
     searchBtnTapped() {
         console.log("搜索" + this.data.searchBarKeyword);

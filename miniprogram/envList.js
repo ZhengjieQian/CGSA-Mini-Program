5a6eccf2667e99ac1e786e44e07838394c29dde3
RatsCommander <jasonjiachengsong@163.com>
2023-06-14 14:43:21 -0700

    Update envList.js

--- miniprogram/envList.js
+++ miniprogram/envList.js
@@ -1,6 +1,6 @@
 // envList.js
 const envList = [{
-  "envId": "cgsa-cloud-9g1n4xrzf93ad387"  // 更改 Cloud 环境时，在这里修改环境 ID（CloudId）
+  "envId": "cgsa-mini-program-9e3o2q71fdb4e3"  // 更改 Cloud 环境时，在这里修改环境 ID（CloudId）
 }];
 module.exports = {
   envList
